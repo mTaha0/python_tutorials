cd371947446ac3c329e2b0b85225b00a7b563be1
mTaha0 <tahatizi25@gmail.com>
2026-08-16 15:36:46 +0300

    feat: add bitwise functions

---
+++ opencv_fundamentals/bitwise.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype="uint8" )
+
+rectangle = cv.rectangle(blank.copy(), (100, 100), (400, 400), 255, -1)
+circle = cv.circle(blank.copy(), (250, 250), 175, 255, -1)
+
+bitwise_and = cv.bitwise_and(rectangle, circle)
+bitwise_or = cv.bitwise_or(rectangle, circle)
+bitwise_xor = cv.bitwise_xor(rectangle, circle)
+bitwise_not = cv.bitwise_not(rectangle)
+
+cv.imshow("rectangle", rectangle)
+cv.imshow("circle", circle)
+cv.imshow("bitwise", bitwise_and)
+cv.imshow("bitwise_or", bitwise_or)
+cv.imshow("bitwise_xor", bitwise_xor)
+cv.imshow("bitwise_not", bitwise_not)
+
+
+cv.waitKey(0)
