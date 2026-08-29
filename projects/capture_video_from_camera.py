98296c60614dcdf2b1a2e5ad9f298cfc4e21407f
mTaha0 <tahatizi25@gmail.com>
2026-08-29 13:35:14 +0300

    feat: add track bar and merged window

--- projects/capture_video_from_camera.py
+++ projects/capture_video_from_camera.py
@@ -1,36 +1,54 @@
 import cv2 as cv
 import numpy as np
 
-alt_sinir = np.array([0, 0, 40])
-üst_sinir = np.array([180, 255, 255])
+#trackbar'ın çalışması için boş fonksiyona ihtiyacımız var
+def bos_fonksiyon(x):
+    pass
+cv.namedWindow('Ayarlar')
+cv.resizeWindow('Ayarlar', 400, 250)
+cv.createTrackbar('H_min', 'Ayarlar', 0, 179, bos_fonksiyon)
+cv.createTrackbar('H_max', 'Ayarlar', 179, 179, bos_fonksiyon)
+cv.createTrackbar('S_min', 'Ayarlar', 0, 255, bos_fonksiyon)
+cv.createTrackbar('S_max', 'Ayarlar', 255, 255, bos_fonksiyon)
+cv.createTrackbar('V_min', 'Ayarlar', 0, 255, bos_fonksiyon)
+cv.createTrackbar('V_max', 'Ayarlar', 255, 255, bos_fonksiyon)
 
+#her bir framedeki kontürleri tespit edip merkezine circle çizme fonksiyonu
 def contour_point(img):
     islenmis_img = img.copy()
     islenmis_img = cv.flip(frame, 1)
     hsv = cv.cvtColor(islenmis_img, cv.COLOR_BGR2HSV)
+    #track bar pozisyonları almak için 
+    h_min = cv.getTrackbarPos('H_min', 'Ayarlar')
+    h_max = cv.getTrackbarPos('H_max', 'Ayarlar')
+    s_min = cv.getTrackbarPos('S_min', 'Ayarlar')
+    s_max = cv.getTrackbarPos('S_max', 'Ayarlar')
+    v_min = cv.getTrackbarPos('V_min', 'Ayarlar')
+    v_max = cv.getTrackbarPos('V_max', 'Ayarlar')
+
+    alt_sinir = np.array([h_min, s_min, v_min])
+    üst_sinir = np.array([h_max, s_max, v_max])
+    #sınır içi değerleri beyaz, kalanı siyah yapmak için
     mask = cv.inRange(hsv, alt_sinir, üst_sinir)
     mask = cv.medianBlur(mask, 5)
-
+    hsv[mask == 0] = [180, 255, 255]
+    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+    #bitwise = cv.bitwise_or(islenmis_img, hsv)
     contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    copy = hsv.copy()
 
     for contour in contours:
-        if cv.contourArea(contour) > 50:
+        if cv.contourArea(contour) > 25:
             M = cv.moments(contour)
 
             if M['m00'] != 0:
                 cX = int(M['m10']/M['m00'])
                 cY = int(M['m01']/M['m00'])
-                cv.circle(copy, (cX, cY), 10, (0, 0, 255), -1)
-                cv.drawContours(copy, [contour], -1, (0, 255, 0), 2)
+                cv.circle(bgr, (cX, cY), 10, (255, 0, 255), -1)
+                cv.drawContours(bgr, [contour], -1, (255, 255, 0), 2)
 
-
-    hsv[mask == 0] = [255, 0, 0]
-
-    return islenmis_img, mask
+    return bgr, mask
 
 cap = cv.VideoCapture(0)
-
 if not cap.isOpened():
     print("Can't opened camera ! Exiting...")
     exit()
@@ -38,7 +56,6 @@ if not cap.isOpened():
 #FourCC, video sıkıştırma formatını (codec) işletim sistemine tanıtan koddur
 fourcc = cv.VideoWriter_fourcc(*'XVID')
 out = cv.VideoWriter('output.avi', fourcc, 30.0, (640,  480))
-
 while cap.isOpened():
     ret, frame = cap.read()
     
@@ -46,14 +63,13 @@ while cap.isOpened():
         print("Cant receive frame ! Exiting...")
         break
 
-    sonuc, maske = contour_point(frame)
-    cv.imshow("sonuc", sonuc)
-    cv.imshow('maske', maske)
+    result, mask = contour_point(frame)
+    mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+    merged_window = np.hstack((result, mask))
+    cv.imshow('Merged Window', merged_window)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv.destroyAllWindows()
-
-
